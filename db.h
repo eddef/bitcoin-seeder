155c1f77af0f7aa93812d040ef70324cb9277e58
Fedde <fredrikbod@gmail.com>
2016-05-07 09:27:15 +0200

    missing }

--- db.h
+++ db.h
@@ -15,10 +15,10 @@
 #define REQUIRE_VERSION 70001
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
-{
- HEAD
-    return testnet ? 0 : 140000;
 
+{
+    return testnet ? 0 : 140000;
+}
 std::string static inline ToString(const CService &ip) {
   std::string str = ip.ToString();
   while (str.size() < 22) str += ' ';
